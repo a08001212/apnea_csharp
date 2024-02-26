f8c3f97285d477c060e3f70a0eb583e0945ec858
= <a08001212@gmail.com>
2024-02-26 12:33:11 +0800

    remove bin

--- apnea/obj/Debug/net8.0/apnea.AssemblyInfo.cs
+++ apnea/obj/Debug/net8.0/apnea.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apnea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d9439bd6684eabc98eb91c809da449b254eab22")]
 [assembly: System.Reflection.AssemblyProductAttribute("apnea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apnea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
