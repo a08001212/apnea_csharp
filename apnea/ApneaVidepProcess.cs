a240d4a6cba67a6ab2faa28b89799456f6e41abb
= <a08001212@gmail.com>
2024-02-29 15:57:06 +0800

    find super pixel.

--- apnea/ApneaVidepProcess.cs
+++ apnea/ApneaVidepProcess.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using OpenCvSharp;
+using OpenCvSharp.XImgProc;
 
 namespace apnea;
 
@@ -14,9 +15,15 @@ public class ApneaVidepProcess
 
     private Rect find_face(Mat<int> frame)
     {
-        var faces = detector.DetectMultiScale(frame, 1.1, 4);
+        // Cv2.ImShow("test", frame);
+        // Cv2.WaitKey();
+        var faces = detector.DetectMultiScale(frame, 2, 4);
+        if (faces.Length == 0)
+        {
+            return new Rect(-1, -1, -1, -1);
+        }
         int ans = 0, max_area = 0;
-        var face = faces[0];
+        // var face = faces[0];
         for (int i = 0; i < faces.Length; ++i)
         {
             var area = faces[i].Height * faces[i].Width;
@@ -26,21 +33,30 @@ public class ApneaVidepProcess
                 max_area = area;
             }
         }
-
-        return faces[max_area];
+        
+        return faces[ans];
     }
     private List<Double> get_breath_data()
     {
+        double last_data = 0.0;
+        List<double> ans = new List<double>();
         Mat<int> frame = new Mat<int>(videoHeight, videoWidth);
-        int count = 0;
         while (videoCapture.Read(frame))
         {
+            double frame_gray_value;
             Cv2.CvtColor(frame, frame, ColorConversionCodes.RGB2GRAY);
             var face = find_face(frame);
-            Rect chest = new Rect(int.Max(face.X - face.Width, 0), int.Min(videoHeight, (int)(face.Y + 1.25 * face.Height)), face.Width * 3, (int)(face
-                .Height * 1.25));
             
-
+            if (face.X == -1 || face.X - (int)(face.Width * 1.2) < 0 ||(int)(face.Y + 1.3 * face.Height)+face.Height*2 >= frame.Height || face.X - (int)(face.Width * 1.2) + 4*face.Width >= frame.Width)
+            {
+                ans.Add(last_data);
+                continue;
+            }
+            Rect chest = new Rect(face.X - (int)(face.Width * 1.2),(int)(face.Y + 1.3 * face.Height), face.Width * 4, face.Height * 2);
+            Mat<int> chest_img = new Mat<int>(frame, chest);
+            var super_pixel = SuperpixelSLIC.Create(chest_img,SLICType.SLICO, 10, 0.075F);
+            // Console.WriteLine($"{super_pixel}");
+            Console.WriteLine(super_pixel.GetNumberOfSuperpixels());
         }
         return new List<double>();
     }
