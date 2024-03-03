34f9213a95426400dc3a53d23a48dca4b998e92e
aacxx <a08001212@gmail.con>
2024-03-03 20:37:37 +0800

    get super pixel value, rewrite some function.

--- apnea/ApneaVideoProcess.cs
+++ apnea/ApneaVideoProcess.cs
@@ -4,16 +4,16 @@ using OpenCvSharp.XImgProc;
 
 namespace apnea;
 
-public class ApneaVidepProcess
+public class ApneaVideoProcess
 {
     private string video_path;
     private double fps;
     private int videoWidth, videoHeight;
-
+    private List<double> rr_rate;
     private VideoCapture videoCapture;
     private CascadeClassifier detector;
-
-    public ApneaVidepProcess(string video_path)
+    private Rect chest_area;
+    public ApneaVideoProcess(string video_path)
     {
         detector = new CascadeClassifier(@"..\..\..\haarcascades\haarcascade_frontalface_alt2.xml");
         this.video_path = video_path;
@@ -27,6 +27,7 @@ public class ApneaVidepProcess
             throw;
         }
 
+        rr_rate = new List<double>();
         videoWidth = (int)videoCapture.Get(VideoCaptureProperties.FrameWidth);
         videoHeight = (int)videoCapture.Get(VideoCaptureProperties.FrameHeight);
 
@@ -35,14 +36,20 @@ public class ApneaVidepProcess
         videoCapture.Release();
     }
 
-
-    private Mat<int> get_frame()
+    void set_video_path(string video_path)
     {
-        Mat<int> frame = new Mat<int>(videoHeight, videoWidth);
-        videoCapture.Read(frame);
-        return frame;
+        this.video_path = video_path;
     }
     
+    public void draw_data(List<Double> arr)
+    {
+        
+    }
+
+    public List<double> get_rr_rate()
+    {
+        return rr_rate;
+    }
     private Rect find_face(Mat<int> frame)
     {
 
@@ -52,7 +59,6 @@ public class ApneaVidepProcess
         {
             return new Rect(-1, -1, -1, -1);
         }
-        
         int ans = 0, max_area = 0;
         for (int i = 0; i < faces.Length; ++i)
         {
@@ -63,59 +69,56 @@ public class ApneaVidepProcess
                 max_area = area;
             }
         }
-        
         return faces[ans];
+    }
+
+    private void get_chest_area(Rect face)
+    {
+        int start_x = int.Max((int)(face.X - face.Width * 1.2), 0);
+        int start_y = int.Min(videoHeight-1, (int)(face.Y + 1.3 * face.Height));
+        int end_x = int.Min(videoWidth-1, (int)(face.Width * 3.5 + start_x)) - start_x;
+        int end_y = int.Min(videoHeight-1, face.Height * 2 + start_y) - start_y;
+        chest_area = new Rect(start_x, start_y, end_x, end_y);
     }
 
     private Mat<int> get_chest_img(Mat<int> frame)
     {
-        var face = find_face(frame);
-        
-        // can't find chest
-        if (face.X == -1 )
-        {
-            return new Mat<int>(0, 0);
-        }
-
-        int start_x = int.Max((int)(face.X - face.Width * 1.2), 0);
-        int start_y = int.Min(frame.Height-1, (int)(face.Y + 1.3 * face.Height));
-        int end_x = int.Min(frame.Width-1, (int)(face.Width * 3.5 + start_x)) - start_x;
-        int end_y = int.Min(frame.Height-1, face.Height * 2 + start_y) - start_y;
-        Rect chest = new Rect(start_x, start_y, end_x, end_y);
-        return new Mat<int>(frame, chest);
+          return new Mat<int>(frame, chest_area);
     }
-    private List<Double> get_breath_data()
+    private void get_breath_data()
     {
+        rr_rate.Clear();
         double last_data = 0.0;
-        List<double> ans = new List<double>();
         Mat<int> frame = new Mat<int>(videoHeight, videoWidth);
         while (videoCapture.Read(frame))
         {
             double frame_gray_value;
-            // Cv2.CvtColor(frame, frame, ColorConversionCodes.RGB2GRAY);
+            Cv2.CvtColor(frame, frame, ColorConversionCodes.RGB2GRAY);
             Mat<int> chest_img = get_chest_img(frame);
             
             // can't find chest
             if (chest_img.Height == 0)
             {
-                ans.Add(last_data);
+                rr_rate.Add(last_data);
                 continue;
             }
             var super_pixel = SuperpixelSLIC.Create(chest_img,SLICType.SLICO, 10, 0.075F);
+            super_pixel.Iterate(10);
             Mat<int> super_pixel_res = new Mat<int>(super_pixel.GetNumberOfSuperpixels(), super_pixel.GetNumberOfSuperpixels());
             super_pixel.GetLabels(super_pixel_res);
-            // Console.WriteLine($"{super_pixel_res}");
-            Cv2.ImShow("test", super_pixel_res);
-            Cv2.WaitKey(1);
+            var data = super_pixel_res.ToArray();
+            int sum = data.Sum();
+            last_data = sum / (double)super_pixel.GetNumberOfSuperpixels();
+            // Console.WriteLine($"{sum / (double)super_pixel.GetNumberOfSuperpixels()}");
+            rr_rate.Add(last_data);
         }
-        return new List<double>();
     }
-
-    
     public double get_fps()
     {
         return fps;
     }
+
+
     
     
 
